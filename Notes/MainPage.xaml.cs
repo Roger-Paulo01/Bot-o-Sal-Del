a8ae1c69a7b7976823b194ae5babba83acd6036b
rafaela-oliveira21 <rafalelaoliveira20@gmail.com>
2025-08-18 22:36:51 -0300

    Adicionei a página de anotações

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string caminho = Path.Combine(FileSystem.AppDataDirectory,"arquivo");
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SalvamentoBtn_Clicked(object sender, EventArgs e)
+        {
+            string conteudo = CaixaEditor.Text;
+            File.WriteAllText(caminho, conteudo);
+        }
+
+        private void EXclusãoBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SalvarBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ApagarBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ Notes/NewPage1.xaml.cs
@@ -0,0 +1,19 @@
+namespace Notes;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+
+    private void EnviarGitBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void EnviarGitBtn_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+}

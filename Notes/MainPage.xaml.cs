dc077475ea9918b5e721c6683e63a850e3937089
rafaela-oliveira21 <rafalelaoliveira20@gmail.com>
2025-08-25 21:45:58 -0300

    métodos salvar e excluir adicionados

--- Notes/MainPage.xaml.cs
+++ Notes/MainPage.xaml.cs
@@ -1,33 +1,42 @@
-﻿namespace Notes
+﻿
+
+namespace Notes
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
         string caminho = Path.Combine(FileSystem.AppDataDirectory,"arquivo");
+        
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(caminho))
+                CaixaEditor.Text = File.ReadAllText(caminho);
         }
-
-        private void SalvamentoBtn_Clicked(object sender, EventArgs e)
+        
+        
+        private void SalvarBtn_Clicked(object sender, EventArgs e)
         {
             string conteudo = CaixaEditor.Text;
             File.WriteAllText(caminho, conteudo);
-        }
 
-        private void EXclusãoBtn_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
-        private void SalvarBtn_Clicked(object sender, EventArgs e)
-        {
-
+            DisplayAlert("Arquivo salvo", Path.GetFileName(conteudo) , caminho, "ok");
         }
 
         private void ApagarBtn_Clicked(object sender, EventArgs e)
         {
+            File.Delete(caminho);
+           if (File.Exists(caminho))
+            {
+                File.Delete(caminho);
+                CaixaEditor.Text = string.Empty;
+            }
 
+           else
+           {
+                DisplayAlert("Arquivo não encontrado", "Não há arquivo para apagar", "OK");
+           }
+           
         }
     }
 
